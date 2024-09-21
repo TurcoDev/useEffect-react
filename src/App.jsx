a84c8494043dcf00cc79f0dd5c0733efd7197c2c
Lucas Salvatori <lsalvatori.hms@gmail.com>
2024-09-20 21:42:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // setTimeout(() => {
+    //   setData(new Date().toString())
+    // }, 3000);
+    // fetch('https://randomuser.me/api/?results=1')
+    // .then(response => response.json())
+    // .then(data2 => setData(JSON.stringify(data2)));
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(data2 => setData(JSON.stringify(data2)));
+  }, [])
+
+  return (
+    <>
+      <p>{ data }</p>
+    </>
+  )
+}
+
+export default App
